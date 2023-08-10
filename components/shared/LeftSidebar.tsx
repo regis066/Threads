70eaeaf98fd735d5e4ec9a2e0ac6c70d57cb126f
Regis066 <mugisharegis72@gmail.com>
2023-08-10 13:55:35 +0200

    Added Topbar

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+function LeftSidebar () {
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+               
+            </div>
+        </section>
+    )
+}
+
+export default LeftSidebar
